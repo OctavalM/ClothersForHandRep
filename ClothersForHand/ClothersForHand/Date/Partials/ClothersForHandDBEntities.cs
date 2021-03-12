e298edc972fbd6a34af9966d5ca02499e534b100
OctavalM <m.akhmetzianovr@gmail.com>
2021-03-12 19:56:48 +0300

    Commit

---
+++ ClothersForHand/ClothersForHand/Date/Partials/ClothersForHandDBEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothersForHand.Date
+{
+	public partial class ClothersForHandDBEntities : DbContext
+	{
+		private static ClothersForHandDBEntities _context;
+
+		public static ClothersForHandDBEntities GetContext()
+		{
+			if (_context == null)
+				_context = new ClothersForHandDBEntities();
+
+			return _context;
+		}
+	}
+}
---
+++ ClothersForHand/ClothersForHand/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ClothersForHand.Date;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClothersForHand
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			var materialsList = ClothersForHandDBEntities.GetContext().Material.Where(x => x.Img != null).ToList();
+
+			foreach (var material in materialsList)
+			{
+				var m = ClothersForHandDBEntities.GetContext().Material.Where(x => x.MaterialID == material.MaterialID).Single();
+
+				m.Image = Encoding.UTF8.GetBytes(m.Img);
+				ClothersForHandDBEntities.GetContext().SaveChanges();
+
+			}
+		}
+	}
+}
