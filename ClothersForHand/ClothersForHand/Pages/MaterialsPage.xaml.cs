7127cead1900acfc20807b6ffa5dc2a5fc9b2b1d
OctavalM <m.akhmetzianovr@gmail.com>
2021-03-14 21:58:08 +0300

    Session2

--- ClothersForHand/ClothersForHand/Pages/MaterialsPage.xaml.cs
+++ ClothersForHand/ClothersForHand/Pages/MaterialsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ClothersForHand.Date;
+using ClothersForHand.Tools;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -22,15 +23,104 @@ namespace ClothersForHand.Pages
 	/// </summary>
 	public partial class MaterialsPage : Page
 	{
+		int actualPage;
+
 		public MaterialsPage()
 		{
 			InitializeComponent();
-			MaterialsLV.ItemsSource = ClothersForHandDBEntities.GetContext().Material.ToList();
 		}
 
 		private void Page_Loaded(object sender, RoutedEventArgs e)
 		{
-			 
+			MaterialTypeCB.ItemsSource = InsertCombobox.CreatedCombobox(ClothersForHandDBEntities.GetContext().MaterialType.ToList(), new MaterialType { MaterialTypeName = "Все типы" });
+		    RefreshMaterials();
+		}
+
+		public void RefreshMaterials()
+		{
+			var filteredMaterials = ClothersForHandDBEntities.GetContext().Material.ToList();
+
+			if (!string.IsNullOrWhiteSpace(MaterialameTB.Text))
+			{
+				var materialName = MaterialameTB.Text;
+				filteredMaterials = filteredMaterials.Where(x => x.MaterialName.ToLower().Contains(materialName.ToLower())).ToList();
+			}
+
+            if (MaterialNameSortCB.SelectedIndex == 1)
+			{
+				CountInStockSortCB.IsEnabled = false;
+				CostSortCB.IsEnabled = false;
+
+				filteredMaterials = filteredMaterials.OrderBy(x => x.MaterialName).ToList();
+			}
+			else if (MaterialNameSortCB.SelectedIndex == 2)
+			{
+				CountInStockSortCB.IsEnabled = false;
+				CostSortCB.IsEnabled = false;
+
+				filteredMaterials = filteredMaterials.OrderByDescending(x => x.MaterialName).ToList();
+			}
+
+			if (CountInStockSortCB.SelectedIndex == 0)
+			{
+				MaterialNameSortCB.IsEnabled = true;
+				CostSortCB.IsEnabled = true;
+
+				filteredMaterials = filteredMaterials.OrderBy(x => x.CountInStock).ToList();
+			}
+			else if (CountInStockSortCB.SelectedIndex == 1)
+			{
+				MaterialNameSortCB.IsEnabled = false;
+				CostSortCB.IsEnabled = false;
+
+				filteredMaterials = filteredMaterials.OrderByDescending(x => x.CountInStock).ToList();
+			}
+
+			if (CostSortCB.SelectedIndex == 0)
+			{
+				MaterialNameSortCB.IsEnabled = true;
+				CountInStockSortCB.IsEnabled = true;
+
+				filteredMaterials = filteredMaterials.OrderBy(x => x.Cost).ToList();
+			}
+			else if (CostSortCB.SelectedIndex == 1)
+			{
+				MaterialNameSortCB.IsEnabled = false;
+				CountInStockSortCB.IsEnabled = false;
+
+				filteredMaterials = filteredMaterials.OrderByDescending(x => x.Cost).ToList();
+			}
+
+			if (MaterialTypeCB.SelectedIndex > 0)
+			{
+				var selectedType = MaterialTypeCB.SelectedItem as MaterialType;
+				filteredMaterials = filteredMaterials.Where(x => x.MaterialTypeID == selectedType.MaterialTypeID).ToList();
+			}
+
+			MaterialsLV.ItemsSource = filteredMaterials.Take(15).ToList(); 
+		}
+
+		private void MaterialNameSortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{ 
+			RefreshMaterials();
+		}
+
+		private void CountInStockSortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			RefreshMaterials();
+
+		}
+
+		private void CostSortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			RefreshMaterials();
+
+		}
+
+		private void MaterialTypeCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			RefreshMaterials();
+
 		}
 	}
 }
---
+++ ClothersForHand/ClothersForHand/Tools/InsertCombobox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothersForHand.Tools
+{
+	class InsertCombobox
+	{
+		public static List<T> CreatedCombobox<T> (List<T> source, T item)
+		{
+			var result = source.ToList();
+			result.Insert(0, item);
+
+			return result;
+		}
+	}
+}
